7b8233ce12e2c246f60014d3fb28d9306422cc15
Julian Garcia <jugar28@gmail.com>
2021-11-12 15:50:25 -0500

    added upload image feature, including middleware for upload and resize. Packages used: jimp, uuid, and multer

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next) {
+      const isPhoto = file.mimetype.startsWith('image/');
+      if(isPhoto) {
+        next(null, true);
+      } else {
+        next({ message: 'That filetype isn\'t allowed!' }, false);
+      }
+    }
+  };
 
 exports.homePage = (req, res) => {
     res.render('index');
@@ -9,9 +25,26 @@ exports.addStore = (req, res) => {
     res.render('editStore', {title: 'Add Store'});
 }
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+    if(!req.file) {
+        next();
+        return;
+    }
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(800, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    // once we have written the photo to our filesystem, keep going!
+    next();
+}
+
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save();
-    req.flash('success', `Sucessfully Created ${store.name}. Care to leave a review?`)
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 }
 
@@ -26,6 +59,7 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+    req.body.location.type = 'Point';
     const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
         new: true, 
         runValidators: true
--- models/Store.js
+++ models/Store.js
@@ -1,46 +1,48 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String, 
-        trim: true, 
-        required: 'Please enter a store name'
-    }, 
-    slug: String, 
-    description: {
-        type: String, 
-        trim: true
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
     },
-    tags: [String],
-    created: {
-        type: Date,
-        default: Date.now 
-    },
-    location:{
-        type:{
-            type: String, 
-            default: 'Point'
-        },
-        coordinates:[{
-            type: Number, 
-            required: 'You must supply coordinates'
-        }], 
-        address:{
-            type: String, 
-            required: 'You must supply an address'
-        }
-    } 
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  },
+  photo: String
 });
 
 storeSchema.pre('save', function(next) {
-    if (!this.isModified('name')) {
-        next();
-        return;
-    }
-    this.slug = slug(this.name);
-    next();
+  if (!this.isModified('name')) {
+    next(); // skip it
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make more resiliant so slugs are unique
 });
 
 module.exports = mongoose.model('Store', storeSchema);
